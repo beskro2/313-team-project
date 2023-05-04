96d8ccefce00ff12878227aad16fc04a4b890cf6
Steven Champagne <Steven.J.Champagne@ndus.edu>
2023-05-04 17:04:53 -0500

    add Car Model and Admin

--- radiator_springs/homepage/models.py
+++ radiator_springs/homepage/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Car(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
