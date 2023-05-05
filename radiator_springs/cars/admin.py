08fad9a19cdc8940fdb3c1d5fce676bd148e640b
Steven Champagne <Steven.J.Champagne@ndus.edu>
2023-05-04 21:45:40 -0500

    events work

--- radiator_springs/cars/admin.py
+++ radiator_springs/cars/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from .models import Car
+from .models import Events
 
 class CarAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'description')
 
 admin.site.register(Car, CarAdmin)
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('id', 'event_name', 'event_detail')
+
+admin.site.register(Events, EventAdmin)
+
--- radiator_springs/cars/models.py
+++ radiator_springs/cars/models.py
@@ -8,3 +8,11 @@ class Car(models.Model):
     def __str__(self):
         return self.name
 
+class Events(models.Model):
+    id = models.AutoField(primary_key=True)
+    event_name = models.CharField(max_length=100)
+    event_detail = models.TextField()
+
+    def __str__(self):
+        return self.event_name
+
