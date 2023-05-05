92aa661c450f172c68161b5b37c7e54cfbab21d8
Steven Champagne <Steven.J.Champagne@ndus.edu>
2023-05-04 23:38:42 -0500

    fixed hours so it displays event cards

--- radiator_springs/cars/views.py
+++ radiator_springs/cars/views.py
@@ -42,8 +42,8 @@ def events(request):
  return render(request, 'events.html', {'events': events})
 
 def hours(request):
-    template = loader.get_template('hours.html')
-    return HttpResponse(template.render())
+     events = Events.objects.all()
+     return render(request, 'hours.html', {'events': events})
 
 def manufacturer(request):
     template = loader.get_template('manufacturer.html')
